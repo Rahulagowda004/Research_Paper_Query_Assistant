b452f647c13ee0a1363dbd35a7a5950eb4acdafd
rahulagowda277 <rahulgowda277@gmail.com>
2024-10-20 11:44:16 +0530

    done with app.py

--- app.py
+++ app.py
@@ -28,4 +28,36 @@ prompt = ChatPromptTemplate(
 )
 
 def create_vector_embeddings():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings = OllamaEmbeddings()
+        st.session_state.loader = PyPDFDirectoryLoader("Data") ##data ingestion
+        st.session_state.docs = st.session.loader.load() #document loading
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap = 200) #chunking
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:50]) #text splitting
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings) #vector embeddings
+
+
+user_prompt = st.text_input("Enter you query regarding the research paper")
+
+if st.button("Document embedding"):
+    create_vector_embeddings()
+    st.write("Vector Database in ready")
     
+import time 
+
+if user_prompt:
+    document_chain = create_stuff_documents_chain(llm,prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retriever_chain = create_retrieval_chain(retriever, document_chain)
+    
+    start = time.process_time()
+    response = retriever_chain.run({'input':user_prompt})
+    print(f"response time: {time.process_time() - start}")
+    
+    st.write(response['answer'])
+    
+    #with a streamlit expander
+    with st.expander("Document similarity search"):
+        for i, doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write('-------------------')
