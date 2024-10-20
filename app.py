9de09bd5c66bb99a0679a90fff2902f6c755d205
rahulagowda277 <rahulgowda277@gmail.com>
2024-10-20 12:04:26 +0530

    switched from ollama to huggingface

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import streamlit as st
 import os 
 from langchain_groq import ChatGroq
-from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_huggingface import HuggingFaceEmbeddings
 
 from dotenv import load_dotenv
 load_dotenv()
 
 groq_api_key = os.getenv('GROQ_API_KEY')
+os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
 
 llm = ChatGroq(api_key = groq_api_key, model_name = "Llama3-8b-8192") # ChatGroq is a wrapper around the GROQ API
+
+embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 prompt=ChatPromptTemplate.from_template(
     """
@@ -30,7 +33,7 @@ prompt=ChatPromptTemplate.from_template(
 
 def create_vector_embeddings():
     if "vectors" not in st.session_state:
-        st.session_state.embeddings = OllamaEmbeddings()
+        st.session_state.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
         st.session_state.loader = PyPDFDirectoryLoader("Data") ##data ingestion
         st.session_state.docs = st.session_state.loader.load() #document loading
         st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap = 200) #chunking
